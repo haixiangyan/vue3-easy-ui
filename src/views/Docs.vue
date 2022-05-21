a7ac2216492bffe6145cadda10fd94bab5d38c95
haixiangyan <Qq680321>
2022-05-21 10:57:33 +0800

    feat: 初始化 aside

--- src/views/Docs.vue
+++ src/views/Docs.vue
@@ -1,20 +1,37 @@
 <template>
 <div>
-  <div class="top-nav">
-    <div class="logo"></div>
-    <div class="menu"></div>
-  </div>
+  <TopNav></TopNav>
 
   <div class="content">
-    <aside>边栏</aside>
+    <aside>
+      <ol>
+        <li>
+          <router-link to="/doc/switch">Switch 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog 组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tabs">Tabs 组件</router-link>
+        </li>
+      </ol>
+    </aside>
     <main>主内容</main>
   </div>
 </div>
 </template>
 
 <script>
+import TopNav from '../components/TopNav.vue';
+
 export default {
-  name: "Docs"
+  name: "Docs",
+  components: {
+    TopNav,
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,49 @@
 <template>
 <div>
-  <div class="top-nav">
-    <div class="logo"></div>
-    <div class="menu"></div>
-  </div>
+  <TopNav></TopNav>
 
   <div class="banner">
     <h1>简单 UI</h1>
     <h2>一个简单的 UI 框架</h2>
     <p class="actions">
-      <a href="">Github</a>
-      <a href="">开始</a>
+      <a href="https://github.com/haixiangyan/vue3-easy-ui" target="_blank">Github</a>
+      <router-link to="/docs">开始</router-link>
     </p>
   </div>
 </div>
 </template>
 
 <script>
+import TopNav from '../components/TopNav.vue';
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    TopNav,
+  },
 }
 </script>
 
 <style scoped lang="scss">
-.top-nav {
-  border: 1px solid red;
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions{
+    padding: 8px 0;
+    a {
+      margin:0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
 }
 </style>
