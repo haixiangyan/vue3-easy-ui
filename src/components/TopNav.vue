0966e9337d28775a69a5d51ab6971dc25509ce57
haixiangyan <Qq680321>
2022-05-21 10:58:37 +0800

    style: 添加 aside css

---
+++ src/components/TopNav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="top-nav">
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopNav"
+}
+</script>
+
+<style scoped lang="scss">
+.top-nav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
--- src/views/Docs.vue
+++ src/views/Docs.vue
@@ -35,6 +35,21 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
 </style>
