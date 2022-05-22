60a29b8b58c2f50c34ae9806f9e642164589b493
haixiangyan <Qq680321>
2022-05-22 21:30:15 +0800

    feat: 添加 visible 属性

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,17 +2,30 @@
   <div>
     <div>Dialog 示例</div>
     <h1>示例1</h1>
-    <Dialog></Dialog>
+    <Button @click="toggle">Toggle</Button>
+    <Dialog :visible="x"></Dialog>
   </div>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue';
 import Dialog from '../lib/Dialog.vue';
+import {ref} from "vue";
 
 export default {
   name: "DialogDemo",
   components: {
+    Button,
     Dialog
+  },
+  setup() {
+    const x = ref(false)
+
+    const toggle = () => {
+      x.value = !x.value
+    }
+
+    return { x, toggle }
   }
 }
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="vue3-dialog-overlay"></div>
-  <div class="vue3-dialog-wrapper">
-    <div class="vue3-dialog">
-      <header>标题 <span class="vue3-dialog-close"></span></header>
-      <main>
-        <p>第一行字</p>
-        <p>第二行字</p>
-      </main>
-      <footer>
-        <Button level="main">OK</Button>
-        <Button>Cancel</Button>
-      </footer>
+  <template v-if="visible">
+    <div class="vue3-dialog-overlay"></div>
+    <div class="vue3-dialog-wrapper">
+      <div class="vue3-dialog">
+        <header>标题 <span class="vue3-dialog-close"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button level="main">OK</Button>
+          <Button>Cancel</Button>
+        </footer>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from "./Button.vue";
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    }
+  },
   components: {
     Button,
   },
