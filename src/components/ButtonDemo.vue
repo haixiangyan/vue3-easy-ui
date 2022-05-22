706fcc52b04843b85b612b3ac3c111ae89860ba4
haixiangyan <Qq680321>
2022-05-22 15:49:44 +0800

    feat: 完成属性绑定

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,7 @@
   <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick">你好</Button>
   </div>
 </template>
 
@@ -13,6 +13,13 @@ export default {
   name: "ButtonDemo",
   components: {
     Button
+  },
+  setup() {
+    const onClick = () => {
+      console.log('hi');
+    }
+
+    return { onClick }
   }
 }
 </script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Button",
+  // 不继承属性
+  inheritAttrs: false,
+  setup(props, context) {
+    const { size, ...rest } = context.attrs;
+
+    return { size, rest }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
