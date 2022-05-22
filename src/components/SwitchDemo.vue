a5fdb522d8b1373b657a5affd3a3478dc8adc722
haixiangyan <Qq680321>
2022-05-22 13:36:41 +0800

    feat: 优化代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Switch v-model:value="y"></Switch>
+    <Switch v-model:value="bool"></Switch>
   </div>
 </template>
 
@@ -14,9 +14,9 @@ export default {
     Switch
   },
   setup() {
-    const y = ref(true);
+    const bool = ref(true);
 
-    return { y }
+    return { bool }
   }
 }
 </script>
