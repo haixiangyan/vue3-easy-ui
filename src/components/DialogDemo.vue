f748e44ee0827f66030fdda15b224d337dedb8b6
haixiangyan <Qq680321>
2022-05-22 21:39:42 +0800

    feat: Dialog 支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -3,7 +3,7 @@
     <div>Dialog 示例</div>
     <h1>示例1</h1>
     <Button @click="toggle">Toggle</Button>
-    <Dialog :visible="x"></Dialog>
+    <Dialog v-model:visible="x" :ok="f1" :cancel="f2"></Dialog>
   </div>
 </template>
 
@@ -25,7 +25,12 @@ export default {
       x.value = !x.value
     }
 
-    return { x, toggle }
+    const f1 = () => {
+      return true;
+    }
+    const f2 = () => {}
+
+    return { x, toggle, f1, f2 }
   }
 }
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,16 +1,16 @@
 <template>
   <template v-if="visible">
-    <div class="vue3-dialog-overlay"></div>
+    <div class="vue3-dialog-overlay" @click="onClickOverlay"></div>
     <div class="vue3-dialog-wrapper">
       <div class="vue3-dialog">
-        <header>标题 <span class="vue3-dialog-close"></span></header>
+        <header>标题 <span class="vue3-dialog-close" @click="close"></span></header>
         <main>
           <p>第一行字</p>
           <p>第二行字</p>
         </main>
         <footer>
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
     </div>
@@ -24,11 +24,41 @@ export default {
     visible: {
       type: Boolean,
       default: false,
-    }
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true,
+    },
+    ok: { type: Function },
+    cancel: { type: Function },
   },
   components: {
     Button,
   },
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false);
+    }
+
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    }
+
+    const ok = () => {
+      if ((props.ok?.()) !== false) {
+        close();
+      }
+      context.emit('ok');
+    }
+    const cancel = () => {
+      context.emit('cancel');
+      close();
+    }
+
+    return { close, onClickOverlay, ok, cancel }
+  }
 };
 </script>
 
