c412eba6944d0da5e254a77c92a8df4d3bcce71d
haixiangyan <Qq680321>
2022-05-23 22:46:26 +0800

    feat: 实现切换动画

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,23 +1,33 @@
 <template>
 <div class="vue3-tabs">
-  <div class="vue3-tabs-nav">
+  <div class="vue3-tabs-nav" ref="container">
     <div
       @click="select(t)"
       class="vue3-tabs-nav-item"
       :class="{ selected: t === selected }"
+      v-for="(t, index) in titles"
       :key="t"
-      v-for="t in titles">
+      :ref="el => { if (el) navItems[index] = el }"
+    >
       {{t}}
     </div>
+    <div class="vue3-tabs-nav-indicator" ref="indicator"></div>
   </div>
   <div class="vue3-tabs-content">
-    <component class="vue3-tabs-content-item" v-for="(c, index) in defaults" :is="c" :key="index" :class="{ selected: c.props.title === selected }" />
+    <component
+      class="vue3-tabs-content-item"
+      v-for="(c, index) in defaults"
+      :is="c"
+      :key="index"
+      :class="{ selected: c.props.title === selected }"
+    />
   </div>
 </div>
 </template>
 
 <script lang="ts">
 import Tab from '../lib/Tab.vue';
+import {onMounted, onUpdated, ref} from "vue";
 
 export default {
   name: "Tabs",
@@ -27,8 +37,37 @@ export default {
     }
   },
   setup(props, context) {
-    const defaults = context.slots.default();
+    const navItems = ref<HTMLDivElement[]>([]);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
 
+    const x = () => {
+      const divs = navItems.value;
+      const result = divs.find(div => div.classList.contains('selected'))
+
+      const { width } = result.getBoundingClientRect();
+
+      indicator.value.style.width = width + 'px';
+
+      // 容器 left
+      const { left: containerLeft } = container.value.getBoundingClientRect();
+      // 左边 nav 的 left
+      const { left: resultLeft } = result.getBoundingClientRect();
+      const left = resultLeft - containerLeft;
+
+      indicator.value.style.left = left + 'px';
+    }
+
+    // 只在第一次渲染执行
+    onMounted(() => {
+      x();
+    });
+
+    onUpdated(() => {
+      x();
+    })
+
+    const defaults = context.slots.default();
     defaults.forEach(tag => {
       if (tag.type !== Tab) {
         throw new Error('Tabs 子标签必须是 Tabs')
@@ -47,7 +86,7 @@ export default {
       context.emit('update:selected', title)
     }
 
-    return { defaults, titles, current, select }
+    return { defaults, titles, current, select, navItems, indicator, container }
   }
 }
 </script>
@@ -62,6 +101,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
       padding: 8px 0;
@@ -75,6 +115,16 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
 
