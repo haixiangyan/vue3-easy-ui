e217f6d067a3f1c682a21d881a006fc2178d9d7c
haixiangyan <Qq680321>
2022-05-23 22:09:03 +0800

    feat: 渲染 Tab

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,30 @@
 <template>
 <div>
-  Tabs 组件
+  <div :key="t" v-for="t in titles">{{t}}</div>
+  <component v-for="(c, index) in defaults" :is="c" :key="index"></component>
 </div>
 </template>
 
 <script>
+import Tab from '../lib/Tab.vue';
+
 export default {
-  name: "Tabs"
+  name: "Tabs",
+  setup(props, context) {
+    const defaults = context.slots.default();
+
+    defaults.forEach(tag => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tabs')
+      }
+    })
+
+    const titles = defaults.map(tag => {
+      return tag.props.title;
+    })
+
+    return { defaults, titles }
+  }
 }
 </script>
 
