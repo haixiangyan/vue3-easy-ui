32c2ca2c08096501e4e7709d99a863f580ef50cf
haixiangyan <Qq680321>
2022-05-23 21:50:45 +0800

    feat: 创建 Tabs 和 Tab 组件

---
+++ src/lib/Tab.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Tab组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tab"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  Tabs 组件
+</div>
+</template>
+
+<script>
+export default {
+  name: "Tabs"
+}
+</script>
+
+<style scoped>
+
+</style>
