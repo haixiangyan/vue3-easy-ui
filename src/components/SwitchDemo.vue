b6377ea3fa9a578f9b81cb94144f7cdba2991494
haixiangyan <Qq680321>
2022-05-24 13:36:40 +0800

    feat: 优化源代码展示

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo />
+        <component :is="Switch1Demo" key="Switch1Demo"></component>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -16,7 +16,7 @@
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch2Demo />
+        <component :is="Switch2Demo" key="Switch2Demo"></component>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -38,9 +38,6 @@ import { ref } from 'vue'
 
 export default {
   components: {
-    Switch1Demo,
-    Switch2Demo,
-    Switch,
     Button
   },
   setup() {
